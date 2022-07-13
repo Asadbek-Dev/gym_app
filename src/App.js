7265721af721af1ce933af202077b8dd47f7f27d
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-07-13 18:16:09 +0500

    added assets folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import { Box } from '@mui/material'
 import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width='400px'>
+      
+    </Box>
   )
 }
 
