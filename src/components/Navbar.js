dd6eff00a4cff550aca9865c2d8f6557950c2786
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-07-14 17:29:14 +0500

    added reponse header's

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {Stack} from '@mui/material';
+
+import Logo from '../assets/images/Logo.png'
+
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Hello</h1>
-    </div>
+    <Stack direction="row" justifyContent="space-around" sx={{gap:{sm}}} >
+        <Link to='/' alt='logo' style={{width:'48px',height:'48px',margin:'0 20px'}}>
+            <img src={Logo}  />
+        </Link>
+        <Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
+            <Link to='/' style={{textDecoration:'none',color:'#3a1212',borderBottom:'3px solid #ff2625'}}>Home</Link>
+            <a href="#exercises" style={{textDecoration:'none',color:'#3a1212'}}>Exercises</a>
+        </Stack>
+    </Stack>
   )
 }
 
