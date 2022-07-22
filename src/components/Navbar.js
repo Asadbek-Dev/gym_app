db606c8a4f90db0d49375abde4de881774a2279f
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-07-22 22:26:24 +0500

    navbar finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {Stack} from '@mui/material';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
 
-import Logo from '../assets/images/Logo.png'
-
+import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
   return (
-    <Stack direction="row" justifyContent="space-around" sx={{gap:{sm}}} >
-        <Link to='/' alt='logo' style={{width:'48px',height:'48px',margin:'0 20px'}}>
-            <img src={Logo}  />
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{ gap: { sm: "122px", xs: "40px" }, mt: { sm: "32px", xs: "20px" },justifyContent:'none' }} px='20px'
+    >
+      <Link
+        to="/"
+        alt="logo"
+        style={{ width: "48px", height: "48px", margin: "0 20px" }}
+      >
+        <img src={Logo} />
+      </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3a1212",
+            borderBottom: "3px solid #ff2625",
+          }}
+        >
+          Home
         </Link>
-        <Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
-            <Link to='/' style={{textDecoration:'none',color:'#3a1212',borderBottom:'3px solid #ff2625'}}>Home</Link>
-            <a href="#exercises" style={{textDecoration:'none',color:'#3a1212'}}>Exercises</a>
-        </Stack>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3a1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
     </Stack>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
